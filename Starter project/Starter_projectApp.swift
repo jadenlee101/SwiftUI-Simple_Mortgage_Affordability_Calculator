9f308a19f18b27e252d152d982bc8ca709e5d506
Jaden Lee <jaden.lee101@gmail.com>
2023-05-05 22:57:40 -0600

    Initial Commit

---
+++ Starter project/Starter_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Starter_projectApp.swift
+//  Starter project
+//
+//  Created by Jaden Lee on 2023-05-05.
+//
+
+import SwiftUI
+
+@main
+struct Starter_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
