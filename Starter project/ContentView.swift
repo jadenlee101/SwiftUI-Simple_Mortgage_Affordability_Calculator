f9fa55f6fec134d705d7383e94155af40d628a4a
Jaden Lee <jaden.lee101@gmail.com>
2023-05-08 01:02:49 -0600

    expense

--- Starter project/ContentView.swift
+++ Starter project/ContentView.swift
@@ -11,47 +11,65 @@ struct ContentView: View {
     @State private var income : Int = 0
     @State private var additional : Int = 0
     @State var totalIncome : Int = 0
+    @State var next = false
     
     var body: some View {
-        
-        ZStack(alignment: .top){
-            Color.white
-                .ignoresSafeArea()
-            ScrollView{
-                Spacer()
-                Text("Simple Mortgage Affordability Calculator")
-                    .foregroundColor(.white)
-                    .background(.black)
-                    .font(.system(size: 33))
-                
-                HStack() {
-                    Text("Enter your income (before tax):")
-                    Text("$")
-                    TextField("Enter here", value: $income, format: .number)
-                }
-                .font(.system(size:33))
-                .padding(30)
-                
-                HStack() {
-                    Text("Additional income? (ex. spouse)")
-                    Text("$")
-                    TextField("Enter here", value: $additional, format: .number)
-                }
-                .font(.system(size:33))
-                .padding(30)
-                
-                Button("Next!"){
-                    totalIncome = income+additional
-                    print(totalIncome)
+        NavigationStack {
+            ZStack(alignment: .top){
+                Color.white
+                    .ignoresSafeArea()
+                ScrollView{
+                    Spacer()
+                    Text("Simple Mortgage Affordability Calculator")
+                        .foregroundColor(.white)
+                        .background(.black)
+                        .font(.system(size: 33))
                     
+                    HStack() {
+                        Text("Enter your income (before tax):")
+                        Text("$")
+                        TextField("Enter here", value: $income, format: .number)
+                    }
+                    .font(.system(size:33))
+                    .padding(30)
+                    
+                    HStack() {
+                        Text("Additional income? (ex. spouse)")
+                        Text("$")
+                        TextField("Enter here", value: $additional, format: .number)
+                        
+                    }
+                    .font(.system(size:33))
+                    .padding(30)
+                    
+                    
+                        Button {
+                            next.toggle()
+                            totalIncome = income + additional
+                            print(totalIncome)
+                        } label: {
+                            Text("Next!")
+                        }
+                        .foregroundColor (.white)
+                        .background(.black)
+                        .font(.system(size: 33))
+                        .navigationDestination(isPresented: $next) {
+                            ExpenseView()
+                        }
+                        .navigationBarTitle("")
+                        .navigationBarHidden(true)
+                    
+                    
+                    
+    
                 }
-                .font(.system(size: 33))
-                .foregroundColor(.white)
-                .background(.black)
-                
-                
             }
         }
+        
+        
+    
+        
+        
     }
     
 }
--- Starter project/ExpenseView.swift
+++ Starter project/ExpenseView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct ExpenseView: View {
+    @State private var entertainment = 0
+    @State private var groceries = 0
+    @State private var carpayments = 0
+    @State private var others = 0
+    @State var totalExpense = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            ZStack {
+                ScrollView{
+                    Text("Enter Your Monthly Expenses")
+                        .foregroundColor(.white)
+                        .background(.black)
+                        .font(.system(size: 33))
+                    
+                    HStack(alignment: .center) {
+                        Text("Car loans, insurance ")
+                        Text("$")
+                        TextField("Enter here", value: $carpayments, format: .number)
+                    }
+                    .font(.system(size:33))
+                    .padding(10)
+                    
+                    HStack() {
+                        Text("Entertainments")
+                        
+                        Text("$")
+                        TextField("Enter here", value: $entertainment, format: .number)
+                    }
+                    .font(.system(size:33))
+                    .padding(10)
+                    
+                    HStack() {
+                        Text("Groceries")
+                        
+                        Text("$")
+                        TextField("Enter here", value: $groceries, format: .number)
+                    }
+                    .font(.system(size:33))
+                    .padding(10)
+                    
+                    HStack() {
+                        Text("Others")
+                        
+                        Text("$")
+                        TextField("Enter here", value: $others, format: .number)
+                    }
+                    .font(.system(size:33))
+                    .padding(10)
+                }
+            }
+        }
+        
     }
 }
 
